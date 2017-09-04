d250bae011a71ad36f0a74899d5bf8a2d5536240
David Serre <david.serre@sap.com>
2017-09-04 09:33:54 +0200

    Removed solutions from exercises2 trainee folder.

--- exercises/exercise2/src/utils/files.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def loadCsv(filename):
-	lines = csv.reader(open(filename, "rb"), delimiter=';')
-	header = next(lines)
-	dataset = list(lines)
-	for i in range(len(dataset)):
-		dataset[i] = [float(x) for x in dataset[i]]
-	return header, dataset
--- exercises/exercise2/src/utils/ml.py
+++
@@ -1,6 +0,0 @@
-def getAccuracy(testSet, predictions):
-	correct = 0
-	for x in range(len(testSet)):
-		if str(testSet[x][-1]) == str(predictions[x]):
-			correct += 1
-	return correct/float(len(testSet))
